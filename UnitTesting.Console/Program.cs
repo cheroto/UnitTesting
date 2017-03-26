a85c96ef260c43658a7042e16d0a39a818009bcb
Pedro <pedro.cheroto@gmail.com>
2017-03-26 18:31:16 -0400

    My third commit. This is gettin boring.

--- UnitTesting.Console/Program.cs
+++ UnitTesting.Console/Program.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-using System.Collections.Generic;
+using UnitTesting.Core;
 
 namespace UnitTesting.Console
 {
@@ -13,16 +13,9 @@ namespace UnitTesting.Console
         {
             //var input = "46621468";
             var input = System.Console.ReadLine();
-            var inputTempArray = input.ToCharArray();
-            var inputArray = new int[8];
-            var sum = 0;
-            for (int i = inputArray.Length -1; i >= 0; i--)
-            {
-                sum += Convert.ToInt32(inputTempArray[i].ToString()) * (inputArray.Length - i + 1);
-            }
-            var remainder = sum % 11;
-            System.Console.WriteLine(remainder);
-
+            var calculator = new VerificationDigitCalculator();
+            var result = calculator.CalculateVerificationDigit(input);
+            System.Console.WriteLine(result);
         }
     }
 }
--- UnitTesting.Core/VerificationDigitCalculator.cs
+++ UnitTesting.Core/VerificationDigitCalculator.cs
@@ -1,24 +1,24 @@
-﻿//using System;
-//using System.Collections.Generic;
-//using System.Linq;
-//using System.Text;
-//using System.Threading.Tasks;
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
 
-//namespace UnitTesting.Core
-//{
-//    class VerificationDigitCalculator
-//    {
-//        public int CalculateVerificationDigit
-//        //var input = "46621468";
-//        var input = System.Console.ReadLine();
-//        var inputTempArray = input.ToCharArray();
-//        var inputArray = new int[8];
-//        var sum = 0;
-//            for (int i = inputArray.Length -1; i >= 0; i--)
-//            {
-//                sum += Convert.ToInt32(inputTempArray[i].ToString()) * (inputArray.Length - i + 1);
-//            }
-//    var remainder = sum % 11;
-//    System.Console.WriteLine(remainder);
-//    }
-//}
+namespace UnitTesting.Core
+{
+    public class VerificationDigitCalculator
+    {
+        public int CalculateVerificationDigit(string input)
+        {
+            var inputTempArray = input.ToCharArray();
+            var inputArray = new int[inputTempArray.Length];
+            var sum = 0;
+            for (int i = inputArray.Length - 1; i >= 0; i--)
+            {
+                sum += Convert.ToInt32(inputTempArray[i].ToString()) * (inputArray.Length - i + 1);
+            }
+            var remainder = sum % 11;
+            return remainder;
+        }
+    }
+}
