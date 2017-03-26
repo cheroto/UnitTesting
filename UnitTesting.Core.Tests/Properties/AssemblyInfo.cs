f6a1c289e916d4817223370bfe7dc35b9fcd8edd
Pedro <pedro.cheroto@gmail.com>
2017-03-26 19:11:04 -0400

    Commit 4

---
+++ UnitTesting.Core.Tests/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("UnitTesting.Core.Tests")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("UnitTesting.Core.Tests")]
+[assembly: AssemblyCopyright("Copyright ©  2017")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("a79459c3-f01d-405d-9de9-c7f67c90fa87")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ UnitTesting.Core.Tests/VerificationDigitCalculatorTest.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using NUnit.Framework;
+using UnitTesting.Core;
+
+namespace UnitTesting.Core.Tests
+{
+    [TestFixture]
+    public class VerificationDigitCalculatorTest
+    {
+        private VerificationDigitCalculator _verificationDigitCalulator;
+
+        [SetUp]
+        public void Setup()
+        {
+            _verificationDigitCalulator = new VerificationDigitCalculator();
+        } 
+
+        [Test]
+        public void CalculateVerificationDigit_Success()
+        {
+
+            //Arrange
+            var input = "46621468";
+            var expecteResult = 6;
+
+            //Act
+            var result = _verificationDigitCalulator.CalculateVerificationDigit(input);
+
+            //Assert
+            Assert.AreEqual(expecteResult, result);
+        }
+    }
+}
