08d119ca080589d7441431ba568baa59c040c944
Pedro <pedro.cheroto@gmail.com>
2017-03-26 21:59:41 -0400

    Commit 5.

--- UnitTesting.Core.Tests/VerificationDigitCalculatorTest.cs
+++ UnitTesting.Core.Tests/VerificationDigitCalculatorTest.cs
@@ -16,18 +16,65 @@ namespace UnitTesting.Core.Tests
         } 
 
         [Test]
-        public void CalculateVerificationDigit_Success()
+        [TestCase("46621468","6")]
+        [TestCase("10907440","3")]
+        [TestCase("45261246","9")]
+        public void CalculateVerificationDigit_Success(string input, string expectedResult)
         {
 
             //Arrange
-            var input = "46621468";
-            var expecteResult = 6;
+
+
+            //Act
+            var result = _verificationDigitCalulator.CalculateVerificationDigit(input);
+
+            //Assert
+            Assert.AreEqual(expectedResult, result);
+        }
+
+        [Test]
+        public void CalculateVerificationDigit_ResultEqualsX()
+        {
+
+            //Arrange
+            var input = "46621425";
+            var expecteResult = "X";
 
             //Act
             var result = _verificationDigitCalulator.CalculateVerificationDigit(input);
 
             //Assert
             Assert.AreEqual(expecteResult, result);
+        }
+
+        [Test]
+        public void CalculateVerificationDigit_InvalidInput()
+        {
+
+            //Arrange
+            var input = "4662t425";
+           
+
+            //Act
+            var ex = Assert.Throws<FormatException>(() => _verificationDigitCalulator.CalculateVerificationDigit(input));
+
+            //Assert
+            Assert.AreEqual("Invalid Input.", ex.Message);
+        }
+
+        [Test]
+        public void CalculateVerificationDigit_InputSizeError()
+        {
+            //Arrange
+            var input = "46621212425";
+            var expectedResult = "Incorrect input size.";
+
+
+            //Act
+            var ex = Assert.Throws<FormatException>(() => _verificationDigitCalulator.CalculateVerificationDigit(input));
+
+            //Assert
+            Assert.AreEqual(expectedResult, ex.Message);
         }
     }
 }
--- UnitTesting.Core/VerificationDigitCalculator.cs
+++ UnitTesting.Core/VerificationDigitCalculator.cs
@@ -8,17 +8,32 @@ namespace UnitTesting.Core
 {
     public class VerificationDigitCalculator
     {
-        public int CalculateVerificationDigit(string input)
+        public string CalculateVerificationDigit(string input)
         {
+            if (input.Length != 8)
+            {
+                throw new FormatException("Incorrect input size.");
+            }
             var inputTempArray = input.ToCharArray();
             var inputArray = new int[inputTempArray.Length];
             var sum = 0;
             for (int i = inputArray.Length - 1; i >= 0; i--)
             {
-                sum += Convert.ToInt32(inputTempArray[i].ToString()) * (inputArray.Length - i + 1);
+                try
+                {
+                    sum += Convert.ToInt32(inputTempArray[i].ToString()) * (inputArray.Length - i + 1);
+                }
+                catch (FormatException)
+                {
+                    throw new FormatException("Invalid Input.");
+                }
             }
             var remainder = sum % 11;
-            return remainder;
+            if (remainder > 9)
+            {
+                return "X";
+            }
+            return remainder.ToString();
         }
     }
 }
